4c2a80cd5a6cb9bf36064397ccd004526a878e41
MatthiasKi <kisselindustries@gmail.com>
2025-10-29 19:36:20 +0100

    Revert "fix: app freeze (ANR) when we check if a model has been downloaded (isModelDownloaded) (#807)"
    This reverts commit 3654338ee1fd1df58aba734ed7b5995983cefed0.

--- packages/google_mlkit_commons/android/src/main/java/com/google_mlkit_commons/GenericModelManager.java
+++ packages/google_mlkit_commons/android/src/main/java/com/google_mlkit_commons/GenericModelManager.java
@@ -1,8 +1,16 @@
 package com.google_mlkit_commons;
 
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 import com.google.mlkit.common.model.DownloadConditions;
 import com.google.mlkit.common.model.RemoteModel;
 import com.google.mlkit.common.model.RemoteModelManager;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
@@ -12,22 +20,13 @@ public class GenericModelManager {
     private static final String DELETE = "delete";
     private static final String CHECK = "check";
 
-    public interface CheckModelIsDownloadedCallback {
-        void onCheckResult(Boolean isDownloaded);
-
-        void onError(Exception e);
-    }
-
     public RemoteModelManager remoteModelManager = RemoteModelManager.getInstance();
+
+    //To avoid downloading models in the main thread as they are around 20MB and may crash the app.
+    private final ExecutorService executorService = Executors.newCachedThreadPool();
 
     public void manageModel(final RemoteModel model, final MethodCall call, final MethodChannel.Result result) {
         String task = call.argument("task");
-
-        if (task == null) {
-            result.notImplemented();
-            return;
-        }
-
         switch (task) {
             case DOWNLOAD:
                 boolean isWifiReqRequired = call.argument("wifi");
@@ -42,20 +41,9 @@ public class GenericModelManager {
                 deleteModel(model, result);
                 break;
             case CHECK:
-                isModelDownloaded(
-                        model,
-                        new CheckModelIsDownloadedCallback() {
-                            @Override
-                            public void onCheckResult(Boolean isDownloaded) {
-                                result.success(isDownloaded);
-                            }
-
-                            @Override
-                            public void onError(Exception e) {
-                                result.error("error", e.toString(), null);
-                            }
-                        }
-                );
+                Boolean downloaded = isModelDownloaded(model);
+                if (downloaded != null) result.success(downloaded);
+                else result.error("error", null, null);
                 break;
             default:
                 result.notImplemented();
@@ -63,56 +51,42 @@ public class GenericModelManager {
     }
 
     public void downloadModel(RemoteModel remoteModel, DownloadConditions downloadConditions, final MethodChannel.Result result) {
-        isModelDownloaded(
-                remoteModel,
-                new CheckModelIsDownloadedCallback() {
-                    @Override
-                    public void onCheckResult(Boolean isDownloaded) {
-                        if (isDownloaded) {
-                            result.success("success");
-                            return;
-                        }
-
-                        remoteModelManager.download(remoteModel, downloadConditions)
-                                .addOnSuccessListener(aVoid -> result.success("success"))
-                                .addOnFailureListener(e -> result.error("error", e.toString(), null));
-                    }
-
-                    @Override
-                    public void onError(Exception e) {
-                        result.error("error", e.toString(), null);
-                    }
-                }
-        );
+        if (isModelDownloaded(remoteModel)) {
+            result.success("success");
+            return;
+        }
+        remoteModelManager.download(remoteModel, downloadConditions).addOnSuccessListener(aVoid -> result.success("success")).addOnFailureListener(e -> result.error("error", e.toString(), null));
     }
 
     public void deleteModel(RemoteModel remoteModel, final MethodChannel.Result result) {
-        isModelDownloaded(remoteModel, new CheckModelIsDownloadedCallback() {
-            @Override
-            public void onCheckResult(Boolean isDownloaded) {
-                if (!isDownloaded) {
-                    result.success("success");
-                    return;
-                }
-                remoteModelManager.deleteDownloadedModel(remoteModel)
-                        .addOnSuccessListener(aVoid -> result.success("success"))
-                        .addOnFailureListener(e -> result.error("error", e.toString(), null));
-            }
-
-            @Override
-            public void onError(Exception e) {
-                result.error("error", e.toString(), null);
-            }
-        });
+        if (!isModelDownloaded(remoteModel)) {
+            result.success("success");
+            return;
+        }
+        remoteModelManager.deleteDownloadedModel(remoteModel).addOnSuccessListener(aVoid -> result.success("success")).addOnFailureListener(e -> result.error("error", e.toString(), null));
     }
 
-    public void isModelDownloaded(RemoteModel model, CheckModelIsDownloadedCallback callback) {
+    public Boolean isModelDownloaded(RemoteModel model) {
+        IsModelDownloaded myCallable = new IsModelDownloaded(remoteModelManager.isModelDownloaded(model));
+        Future<Boolean> taskResult = executorService.submit(myCallable);
         try {
-            remoteModelManager.isModelDownloaded(model)
-                    .addOnFailureListener(callback::onError)
-                    .addOnSuccessListener(callback::onCheckResult);
-        } catch (Exception e) {
-            callback.onError(e);
+            return taskResult.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
         }
+        return null;
+    }
+}
+
+class IsModelDownloaded implements Callable<Boolean> {
+    final Task<Boolean> booleanTask;
+
+    public IsModelDownloaded(Task<Boolean> booleanTask) {
+        this.booleanTask = booleanTask;
+    }
+
+    @Override
+    public Boolean call() throws Exception {
+        return Tasks.await(booleanTask);
     }
 }
